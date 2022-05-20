e77d9cf1a5a9708fdd84b5716377b72d8f076c13
Kasra <kasra.sorouri@gmail.com>
2022-05-21 01:33:58 +0300

    verson 1

---
+++ api/index Exercise Part 3-4.js
@@ -0,0 +1,87 @@
+console.log('Hello world !')
+let persons = [
+    {
+      name: 'Arto Hellas',
+      number: '040-123456',
+      id: 1
+    },
+    {
+      name: 'Martti Tienari',
+      number: '040-123456',
+      id: 2
+    },
+    {
+      name: 'Arto Jarvinen',
+      number: '040-123456',
+      id: 3
+    },
+    {
+      name: 'Lea Kutvonen',
+      number: '040-123456',
+      id: 4
+    }
+  ];
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.get('/api',(req,res)=>{
+    res.send('<h1>Hello world !</h1>')
+})
+
+app.get('/api/persons',(req,res)=>{
+    res.json(persons)
+})
+
+app.get('/api/persons/:id',(req,res)=>{
+    console.log('res ->', res)
+    console.log('req.param ->', res)
+
+    const id=Number(req.params.id)
+    const person=persons.find(person => person.id===id)
+    if(person){
+        res.json(person)
+    }else{
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    persons=persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+
+const genrateId= ()=>{
+    const genId = Math.random()*10000000
+    return Math.floor(genId)
+}
+
+app.post('/api/persons',(req,res)=>{
+    const body=req.body
+    console.log('body ->', body)
+
+    if (body.name === undefined ){
+        return res.status(400).json({error:'name missing !'})
+    }
+    const person = {
+        name:body.name,
+        number:body.number,
+        id:genrateId()
+    }
+    console.log('new person ->')
+    persons=persons.concat(person)
+    console.log(person);
+    res.json(persons)
+})
+
+
+
+const port=3001
+app.listen(port ,()=>{
+console.log(`The server are running on the port ${port}`)
+})
